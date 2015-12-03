e756ca2167a0c199a741f7e6209c71da52b91379
raulgarciade <garcia.de.la.fuente.raul@gmail.com>
2015-12-03 12:05:36 +0100

    r

--- stecnico/funciones/funciones.php
+++ stecnico/funciones/funciones.php
@@ -1,50 +1,49 @@
  <script language="javascript" src="js/jquery.js"></script>
 
   <div name="formula predictiva">
-      <?php #busqueda predictiva
+      <?php 
+      #busqueda predictiva
       include("conexion.php");//se incluyen los datos para realizar la conexion a su base de datos
 
         //arrays
 
       $arr_Tecnicos_orig = array("RAUL", "JORGE", "DAVID", "JOSE","");
-  $arr_sop = array("PRESENCIAL","REMOTO");  
+      $arr_sop = array("PRESENCIAL","REMOTO");  
 
       $con = "select cliente from clientes";//consulta para seleccionar las palabras a buscar, esto va a depender de su base de datos
       $query = mysqli_query($conexion,$con);
       	?>
           
           <script type="text/javascript">
-        function abrirVentana(url) {
-      	  window.open(url, "nuevo", "directories=no,location=no, menubar=no, scrollbars=yes, statusbar=no, tittlebar=no, width=809, height=250");
-      	
-      }
-      	$(function() {
-      		
-      		<?php
-      		
-      		while($row= mysqli_fetch_array($query)) {//se reciben los valores y se almacenan en un arreglo
-            $elementos[]= '"'.$row['cliente'].'"';
-      	  
-      }
-      $arreglo= implode(", ", $elementos);//junta los valores del array en una sola cadena de texto
+                function abrirVentana(url) {
+              	  window.open(url, "nuevo", "directories=no,location=no, menubar=no, scrollbars=yes, statusbar=no, tittlebar=no, width=809, height=250");
+              	
+                }
+          	     $(function() {
+          		
+              	<?php
+              		
+              		while($row= mysqli_fetch_array($query)) {//se reciben los valores y se almacenan en un arreglo
+                    $elementos[]= '"'.$row['cliente'].'"';
+              	  
+                  }
+                  $arreglo= implode(", ", $elementos);//junta los valores del array en una sola cadena de texto
 
-      		?>	
-      		var availableTags=new Array(<?php echo $arreglo; ?>);//imprime el arreglo dentro de un array de javascript
-      				
-      		$( "#tags" ).autocomplete({
-      			source: availableTags
-      		});
-      		
-      	});
-
-  	</script>
+            		?>	
+              		var availableTags=new Array(<?php echo $arreglo; ?>);//imprime el arreglo dentro de un array de javascript
+              				
+              		$( "#tags" ).autocomplete({
+              			source: availableTags
+              		});
+              		
+              	});
+          </script>
   </div>
   <!--cerrar formula predictiva-->
 
   <?php
   ## zona horaria por defecto
   date_default_timezone_set("Europe/Madrid");
-
   ?>
   <!--no enviar formulario con enter-->
   <script language="javascript">
@@ -68,20 +67,11 @@
   <div name="cabecera">
       <!--PARTE SUPERIOR-->
 
-      <?PHP
-      #Conectamos con MySQL
-      			$conexion = mysqli_connect("localhost","select","123456")
-      				or die ("Fallo en el establecimiento de la conexión");
-
-
-      #Seleccionamos la base de datos a utilizar
-      			mysqli_select_db($conexion,"microtex")
-      			or die("Error en la selección de la base de datos");
-      			
+      <?PHP      			
        #comprovamos servicios activos actuales
-      $result1 = mysqli_query ($conexion,"SELECT * FROM servicios where Estado is not NULL")
-      			or die("Error en la consulta SQL");
-      $srva=0;
+        $result1 = mysqli_query ($conexion,"SELECT * FROM servicios where Estado is not NULL")
+        			or die("Error en la consulta SQL");
+        $srva=0;
 
       while( $row = mysqli_fetch_array ( $result1 )) {
       	$row [ "NUM_SERVICIO" ];
@@ -147,7 +137,7 @@
          activ($page,"busquedas");   echo "<a href='BUSQUEDAS.PHP'><span>BUSQUEDAS</span></a></li>";
          activ($page,"servicios2.0");   echo "<a  href='/servicios2.0/menu.html'><span>MICRO-TEX</span></a></li>";
          activ($page,"servicioactivo");   echo "<a  href='#' onClick='abrirVentana(`CREARSERVACTIVO.PHP`)'><span>NUEVO SERV ACTIVO</span></a></li>";
-        activ($page,"servicios2.0");   echo "<a  href='funciones/destruir.php'><span>cerrar sesion</span></a></li>";   
+         activ($page,"servicios2.0");   echo "<a  href='funciones/destruir.php'><span>cerrar sesion</span></a></li>";   
     echo "<a href='SERVICIOSACTIVOS.php'><img  src='images/srvactivos/"; echo $srva; echo ".png' width='120px' height='50px'></a></ul>";
          
 }
