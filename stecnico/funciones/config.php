930c9b1186ed54467199c1a0ff7cd034a04f976d
raulgarciade <garcia.de.la.fuente.raul@gmail.com>
2016-07-04 10:37:20 +0100

    conf

--- stecnico/funciones/config.php
+++ stecnico/funciones/config.php
@@ -1,22 +1,14 @@
 <?php
 //#### ERRORES
-
 //Gestion de errores
 define('REPORT',true);
-
 //reportes por log, on activo true, false
 define('LOGS',false);
-
-
 //#### LOGIN
 //ligin log, on activo true, false
-
 define('LOGIN',false);
 //array de soporte
-
-
 //## exstras
-
 // filas mostradas en el index
 define('FILASINDEX',20);
 // BORRAR SERVICIOS
