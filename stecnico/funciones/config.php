e910166e9be037709216aa410ff3420c2807eba1
raul garcia de la fuente <garcia.de.la.fuente.raul@gmai.com>
2016-08-21 14:43:59 +0200

    Create config.php

---
+++ stecnico/funciones/config.php
@@ -0,0 +1,21 @@
+<?php
+//#### ERRORES
+//Gestion de errores
+define('REPORT',false);
+//reportes por log, on activo true, false
+define('LOGS',false);
+//#### LOGIN
+//login log, on activo true, false
+define('LOGIN',false);
+//array de soporte
+//## exstraso
+// filas mostradas en el index
+define('FILASINDEX',5);
+// BORRAR SERVICIOS
+	//nivel minimo de borrado, si no hay login y quieres borrar poner nivel de borrado a 0
+	//nivel 0 cualquier usuario, nivel 1 solo admin admin,2 no permite borrar a nadie
+define('NIVELBORRAR',0);
+//
+define('RELOGTIME',60);
+define('RECHAGE',false);
+?>
