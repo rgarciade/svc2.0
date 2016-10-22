2cc842a3ec77d75b7de65fa55f3986d061df1e59
raul garcia de la fuente <garcia.de.la.fuente.raul@gmail.com>
2016-10-22 16:28:04 +0200

    consulta preparada inser

--- ajax/conectar.php
+++ ajax/conectar.php
@@ -4,9 +4,16 @@ $nombre = $_POST['nombre'];
 $texto = $_POST['texto'];
 
 
+$consulta = "INSERT INTO `servicios`(`N_CLIENTE`,`TEXTO`) VALUES (?,?)";
+$sentencia = mysqli_prepare($conexion, $consulta);
 
-mysqli_query ($conexion,"INSERT INTO `servicios`(`N_CLIENTE`,`TEXTO`) VALUES ('$nombre','$texto')")
-					or die("Error en la consulta SQL2");
+mysqli_stmt_bind_param($sentencia, "ss", $nombre, $texto);
+
+/* Ejecutar la sentencia */
+mysqli_stmt_execute($sentencia)or die("Error en la consulta SQL2");
+
 echo "200";
+/* cerrar la sentencia */
+mysqli_stmt_close($sentencia);
 
  ?>
